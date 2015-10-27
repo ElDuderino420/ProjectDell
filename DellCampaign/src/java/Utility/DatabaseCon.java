1c2ac42029da0f395e04e142897352c20fc2ed99
Thug-Lyfe <cph-mb366@cphbusiness.dk>
2015-10-27 11:48:49 +0100

    DatabaseCon

---
+++ DellCampaign/src/java/Utility/DatabaseCon.java
@@ -0,0 +1,54 @@
+package Utility;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author butwhole
+ */
+public class DatabaseCon {
+            private static String driver="com.mysql.jdbc.Driver";
+            private static String url="jdbc:mysql://localhost:3306/DellCampaigns";
+            private static String user="DellHost"; 
+            private static String password="pass1234";
+        private Connection con;
+
+    //-- Singleton ---- 
+    private static DatabaseCon instance;
+    private DatabaseCon(){
+        try
+        {
+            Class.forName(driver);
+            con = DriverManager.getConnection(url, user, password);   // The connection will be released upon program 
+		  					      // termination by the garbage collector	
+        } catch (Exception e)
+        {
+            System.out.println("\n*** Remember to insert your Oracle ID and PW in the DBConnector class! ***\n");
+            System.out.println("error in DBConnector.getConnection()");
+            System.out.println(e);
+        }
+    }
+    public static DatabaseCon getInstance()
+    {
+        if (instance == null)
+            instance = new DatabaseCon();
+        return instance;
+    }
+    //------------------
+    
+    public Connection getConnection()
+    {
+      return con;
+    }
+}
--- DellCampaign/web/index.jsp
+++ DellCampaign/web/index.jsp
@@ -5,13 +5,16 @@
 --%>
 
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@page import="java.util.*" %>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@taglib uri="http://java.sun.com/jsp/jstl/sql" prefix="sql" %>
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>Dell Industries</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        
     </body>
 </html>
